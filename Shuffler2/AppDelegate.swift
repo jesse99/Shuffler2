2e84e22da3f07c5c0983bfb2d900d8b61a0071c7
Jesse Jones <jesse9jones@gmail.com>
2018-07-11 20:30:21 -0700

    Added a log as well as a notification for errors

--- Shuffler2/AppDelegate.swift
+++ Shuffler2/AppDelegate.swift
@@ -2,25 +2,43 @@
 //  Copyright © 2018 MushinApps. All rights reserved.
 import Cocoa
 
+let picturesDir = "/Users/jessejones/Source/Shuffler2/pictures"
+
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
+    override init() {
+        let logPath = "\(picturesDir)/shuffler2.log"
+        
+        let fm = FileManager.default
+        fm.createFile(atPath: logPath, contents: nil, attributes: nil)
+        logFile = FileHandle.init(forWritingAtPath: logPath)!
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        NSUserNotificationCenter.default.delegate = self
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
     
+    func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
+        return true
+    }
+    
     func info(_ text: String) {
-        print(text)
+        logLine(text)
     }
     
     func warn(_ text: String) {
-        print("warning:" + text)
+        logLine("warning: " + text)
     }
     
     func error(_ text: String) {
-        print("error:" + text)
+        logLine("error: " + text)
+        logFile.synchronizeFile()
+        
+        notifyUser(text)
     }
     
     // It'd be nicer to put these on the ImageViewController but that's a background sort of
@@ -34,6 +52,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     var imageView: ImageViewController! = nil
-    let store = FileSystemStore.init("/Users/jessejones/Source/Shuffler2/pictures")
+    let store = FileSystemStore.init(picturesDir)
+    
+    private func logLine(_ text: String) {
+        print(text)
+        
+        let data = text.data(using: .utf8)
+        logFile.write(data!)
+        
+        let data2 = "\n".data(using: .utf8)
+        logFile.write(data2!)
+    }
+
+    private func notifyUser(_ text: String) {
+        let notification = NSUserNotification()
+        notification.identifier = "shuffler-error2"
+        notification.title = "Shuffler2 Error"
+        notification.informativeText = text
+        notification.soundName = NSUserNotificationDefaultSoundName
+        notification.hasActionButton = false
+
+        let center = NSUserNotificationCenter.default
+        center.delegate = self
+        center.deliver(notification)
+    }
+    
+    private var logFile: FileHandle
 }
 
